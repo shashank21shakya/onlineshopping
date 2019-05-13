fb84f8df67ed2a601b2809e46fe3b0f8ac499ccd
shashank <shashank.shakya@yash.com>
2019-05-13 15:26:34 +0530

    edit in controller

--- onlineshopping/src/main/java/com/yash/onlineshopping/controller/pageController.java
+++ onlineshopping/src/main/java/com/yash/onlineshopping/controller/pageController.java
@@ -17,19 +17,7 @@ public class pageController {
 		return mv;
 
 	}
-/*	
-	@RequestMapping(value = {"/test"})
-	public ModelAndView test(@RequestParam(value ="greeting",required= false) String greeting) {
-		if(greeting == null) {
-			greeting ="Hellow there";
-		}
-		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", greeting);
-		return mv;
-		
-		
-	}*/
-	
+
 	@RequestMapping(value = {"/test/{greeting}"})
 	public ModelAndView test(@PathVariable(value ="greeting",required=false) String greeting) {
 		if(greeting == null) {

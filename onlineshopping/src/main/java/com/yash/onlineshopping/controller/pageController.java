e4d482b9265e0b76635be5535006727e39f06678
shashank <shashank.shakya@yash.com>
2019-05-13 15:14:28 +0530

    Static resources uploaded

---
+++ onlineshopping/src/main/java/com/yash/onlineshopping/controller/pageController.java
@@ -0,0 +1,44 @@
+package com.yash.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class pageController {
+
+	@RequestMapping(value = { "/", "/home", "/index" })
+	public ModelAndView index() {
+
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", "Welcome to web mvc");
+
+		return mv;
+
+	}
+/*	
+	@RequestMapping(value = {"/test"})
+	public ModelAndView test(@RequestParam(value ="greeting",required= false) String greeting) {
+		if(greeting == null) {
+			greeting ="Hellow there";
+		}
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", greeting);
+		return mv;
+		
+		
+	}*/
+	
+	@RequestMapping(value = {"/test/{greeting}"})
+	public ModelAndView test(@PathVariable(value ="greeting",required=false) String greeting) {
+		if(greeting == null) {
+			greeting ="Hellow there";
+		}
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", greeting);
+		return mv;
+		
+		
+	}
+}

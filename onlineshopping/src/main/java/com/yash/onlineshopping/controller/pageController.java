7050efe7cb9f0cbcebcf21e47716628e65749681
shashank <shashank.shakya@yash.com>
2019-05-20 16:46:34 +0530

    new commit has been done

--- onlineshopping/src/main/java/com/yash/onlineshopping/controller/pageController.java
+++ onlineshopping/src/main/java/com/yash/onlineshopping/controller/pageController.java
@@ -14,7 +14,7 @@ public class pageController {
 		
 		mv.addObject("title", "Home");
 		mv.addObject("userClickHome", true);
-		System.out.println("sjajs");
+		System.out.println("sdasd");
 
 		return mv;
 
